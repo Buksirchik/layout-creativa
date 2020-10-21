f63ea1d546b79759faab749a9b328961a438a7e1
Ruslan Buloychik <rusbuloichyk@gmail.com>
2020-10-21 23:34:01 +0300

    fixed bugs in IE11

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     appendDots: ".control-block-wrap",
     dotsClass: "control-block",
     dots: true,
-    customPaging: function(slider, index) {
+    customPaging: function (slider, index) {
       return '<button class="control__btn">' + btnArr[index] + "</button>";
     },
   });
@@ -24,35 +24,51 @@ $(document).ready(function () {
     dots: true,
     appendDots: ".clients-slider .slider-control-wrap",
     dotsClass: "clients-slider__control",
-    customPaging: function() {
+    customPaging: function () {
       return '<button class="clients-slider__btn"></button>';
-    }
+    },
   });
 
   // tabs
 
-  const tabs = document.querySelector('.tabs');
-  tabs.addEventListener('click', toggleTab)
+  const tabs = document.querySelector(".tabs");
+  tabs.addEventListener("click", toggleTab);
 
   function toggleTab(e) {
-    const tabHeader = e.target.closest(".tab__header")
+    const tabHeader = e.target.closest(".tab__header");
 
-    if (!tabHeader) return
+    if (!tabHeader) return;
 
-    tabHeader.classList.toggle("active-tab")
+    tabHeader.classList.toggle("active-tab");
   }
 
-  // load news 
+  // load news
+  
+  if (!Element.prototype.matches) {
+    Element.prototype.matches =
+      Element.prototype.msMatchesSelector ||
+      Element.prototype.webkitMatchesSelector;
+  }
+  if (!Element.prototype.closest) {
+    Element.prototype.closest = function (s) {
+      var el = this;
 
-  const btn = document.querySelector('.load-more')
-  const container = document.querySelector('.blog-content')
-  const blogs = document.querySelectorAll(".blog-item")
+      do {
+        if (Element.prototype.matches.call(el, s)) return el;
+        el = el.parentElement || el.parentNode;
+      } while (el !== null && el.nodeType === 1);
+      return null;
+    };
+  }
 
-  btn.onclick = function() {
+  const btn = document.querySelector(".load-more");
+  const container = document.querySelector(".blog-content");
+  const blogs = document.querySelectorAll(".blog-item");
+
+  btn.onclick = function () {
     for (let i = 0; i < blogs.length; i++) {
-      const cloneItem = blogs[i].cloneNode(true)
-      container.append(cloneItem)
+      const cloneItem = blogs[i].cloneNode(true);
+      container.appendChild(cloneItem);
     }
-  }
+  };
 });
-
